93e9144b1c5328410e44721ae384e840856d8463
OTHMANE - SASS <oth.lahrimi1996@gmail.com>
2025-09-10 11:29:31 +0000

    add hello

--- routers/statistics.router.js
+++ routers/statistics.router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getAllStatistics } = require("../controllers/statistics.controller");
 const routes = "routes";
+const hello = "hello";
 
 router = express.Router()
 
